e6fa63d1e38e100f2690a04dd61deec02514b718
D.Butramyou <d.butramyou@gmail.com>
2021-05-20 17:05:07 +0300

    Created inventory listener

---
+++ src/main/java/home/sfg/common/events/BeerDto.java
@@ -0,0 +1,55 @@
+package home.sfg.common.events;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class BeerDto implements Serializable {
+
+  static final long serialVersionUID = -8150796741030672486L;
+
+  @Null
+  private UUID id;
+
+  @Null
+  protected Integer version;
+
+  @Null
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
+  private OffsetDateTime createdDate;
+
+  @Null
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
+  private OffsetDateTime lastModifiedDate;
+
+  @NotBlank
+  private String beerName;
+
+  @NotNull
+  private String beerStyle;
+
+  @NotNull
+  private String upc;
+
+  @Positive
+  @NotNull
+  @JsonFormat(shape = JsonFormat.Shape.STRING)
+  private BigDecimal price;
+
+  private Integer quantityOnHand;
+}
---
+++ src/main/java/home/sfg/common/events/BeerEvent.java
@@ -0,0 +1,19 @@
+package home.sfg.common.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BeerEvent implements Serializable {
+
+  static final long serialVersionUID = -4844104841230020484L;
+
+  private BeerDto beerDto;
+}
